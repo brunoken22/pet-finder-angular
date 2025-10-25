3196451f38694a420d98be0303a12a782bef9bc8
brunoken22 <bruno_am_22@hotmail.com>
2025-10-24 21:53:38 -0300

    refactor: Remove unused localStorageService and handleDeletePet method from MyReportPage

--- src/app/pages/user/myReport/myReport.ts
+++ src/app/pages/user/myReport/myReport.ts
@@ -1,7 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { PetServices } from '../../../core/services/pets.service';
 import { PetCardComponent } from '../../../components/petCard/petCard';
-import { LocalStorageService } from '../../../core/services/local-storage.service';
 import { SkeletonCardPet } from '../../../components/skeletonCardPet/skeletonCardPet';
 
 @Component({
@@ -10,33 +9,11 @@ import { SkeletonCardPet } from '../../../components/skeletonCardPet/skeletonCar
 })
 export class MyReportPage {
   private petServices = inject(PetServices);
-  private localStorageService = inject(LocalStorageService);
-
   loading = true;
+
   get pets() {
     const pets = this.petServices.get();
     this.loading = false;
     return pets;
-  }
-
-  async handleDeletePet(id: string): Promise<any> {
-    console.log('Eliminando mascota con ID 2 :', id);
-
-    const token = this.localStorageService.getItem('LOGIN_PET_FINDER');
-    if (!token) return;
-
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        console.log('Eliminando mascota con ID:', id);
-
-        // Aquí tu lógica real comentada:
-        // const responseDeletePet = await this.petServices.deletePet(id, token);
-        // if (responseDeletePet.petRes) {
-        //   this.petServices.pets.update((pets) => pets.filter((pet) => pet.id != id));
-        // }
-        console.log('dasda');
-        resolve({ success: true, message: 'Mascota eliminada', id });
-      }, 5000);
-    });
   }
 }
