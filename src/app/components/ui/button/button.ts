3574568a4c404904b0d202aa9036d484f48c3f62
brunoken22 <bruno_am_22@hotmail.com>
2025-10-24 21:48:39 -0300

    feat: Enhance button component with loading state and additional icons for session and report actions

--- src/app/components/ui/button/button.ts
+++ src/app/components/ui/button/button.ts
@@ -1,12 +1,15 @@
 import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroInformationCircle, heroTrash } from '@ng-icons/heroicons/outline';
 
 type ButtonType = 'default' | 'green' | 'red' | 'purple' | '';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.html',
-  imports: [NgClass],
+  imports: [NgClass, NgIcon],
+  viewProviders: [provideIcons({ heroTrash, heroInformationCircle })],
 })
 export class ButtonComponent {
   @Input() title = '';
