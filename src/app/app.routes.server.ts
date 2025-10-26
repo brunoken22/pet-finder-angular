13e09d02ff3fe96e1b69556dfaca0a4555ae5ed3
brunoken22 <bruno_am_22@hotmail.com>
2025-10-26 18:08:40 -0300

    fix: Uncomment user report modification route in server routes configuration

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,10 +1,10 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  // {
-  //   path: '/user/modifyReport/:id',
-  //   renderMode: RenderMode.Server,
-  // },
+  {
+    path: 'user/modifyReport/:id',
+    renderMode: RenderMode.Server,
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender,
