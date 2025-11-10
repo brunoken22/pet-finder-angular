8330cd5b8b6c727883eca8ba32c731384ca8811f
brunoken22 <bruno_am_22@hotmail.com>
2025-11-10 13:12:06 -0300

    probando

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,6 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,9 +15,5 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch()),
-    {
-      provide: LocationStrategy,
-      useClass: PathLocationStrategy,
-    },
   ],
 };
